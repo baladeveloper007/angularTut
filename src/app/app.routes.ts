bb6b8b712a93a005bd4b040654b87c1282df6302
baladeveloper007 <Bala.dev@hotmail.com>
2024-10-20 19:21:06 +0530

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'first', component: FirstComponent},
+    { path: 'second', component: SecondComponent},
+    {path: '', redirectTo: 'first', pathMatch: 'full'}
+];
